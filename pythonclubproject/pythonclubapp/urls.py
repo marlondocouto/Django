8a670cb65871d51c444aa8bfe3a525ca6878f733
Marlon Do Couto <59633099+marlondocouto@users.noreply.github.com>
2020-02-17 09:15:56 -0800

    Add files via upload

--- pythonclubproject/pythonclubapp/urls.py
+++ pythonclubproject/pythonclubapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #empty quotes work only for index pages. 
+    path('getResources/', views.getResources, name='resources'),
 ]
+
--- pythonclubproject/pythonclubapp/views.py
+++ pythonclubproject/pythonclubapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event 
 
 # Create your views here.
 def index(request):
     #when requests comes in, this function says where to find the page:
     return render(request, 'pythonclubapp/index.html')
     
+def getResources(request): #request is browser asking for a page
+    resource_list=Resource.objects.all()#all will return everything but there are other ways to do
+    context={'resource_list' : resource_list}#dictionary: set up context: pass the list to the webpage
+    return render(request, 'pythonclubapp/resources.html', context=context) #this could be just the curly braces, you basically set up a variable above
+    #the name of html page has to match exactly the page inside the templates/pythonclubapp
